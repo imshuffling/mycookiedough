7c7d90b657c0620e034abe767550701f43f96bf0
David Riches <hi@davidrich.es>
2019-07-16 17:24:01 +0100

    updating playinline

--- src/content-modules/blockvideo/index.js
+++ src/content-modules/blockvideo/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BlockVideo({ video }) {
   return (
     <section>
-          <video className="hero" playInline autoPlay muted>
+          <video className="hero" playinline autoPlay muted>
             <source src={video.file.url} type="video/mp4" />
           </video>
     </section>
